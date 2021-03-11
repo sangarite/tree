a19726021ffe68448c88141dfaacd77048b365e6
sangarite <70514899+onetimejob@users.noreply.github.com>
2021-03-11 20:30:17 +0200

    addin existing files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Pedigree from './Pedigree';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Pedigree />
   </React.StrictMode>,
   document.getElementById('root')
 );
